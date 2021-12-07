4126e5a1992ead5cbb773b1cf81135d23477f4ca
James Cooper <j.haddon.cooper@gmail.com>
2021-12-07 19:46:32 +0000

    add solution to day 5 part 2 in lua

--- Day5/day5.lua
+++ Day5/day5.lua
@@ -40,9 +40,9 @@ local function generate_map_from_lines(lines)
    end
 
    local map = {}
-   for i=1, global_highest_x, 1 do
+   for i=1, global_highest_y, 1 do
       local row = {}
-      for k=1, global_highest_y, 1 do
+      for k=1, global_highest_x, 1 do
          table.insert(row,k,0)
       end
       table.insert(map,i,row)
@@ -54,26 +54,55 @@ end
 -- Add a single straight line to the map
 local function add_line_to_map(line,map)
    local is_horizontal = line[1] == line[3]
+   local is_vertical = line[2] == line[4]
    if is_horizontal then
       local min_y = math.min(line[2], line[4])
       local max_y = math.max(line[2], line[4])
       for i=min_y, max_y, 1 do
-         map[line[1]][i] =  map[line[1]][i] + 1
+         map[i][line[1]] =  map[i][line[1]] + 1
       end
-   else
+   elseif is_vertical then
       local min_x = math.min(line[1], line[3])
       local max_x = math.max(line[1], line[3])
       for i=min_x, max_x, 1 do
-         map[i][line[2]] =  map[i][line[2]] + 1
+         map[line[2]][i] =  map[line[2]][i] + 1
+      end
+   else 
+      local current_x =line[1]
+      local current_y =line[2]
+      local end_x =line[3]
+      local end_y =line[4]
+
+      local is_x_increasing = current_x < end_x
+      local is_y_increasing = current_y < end_y
+
+      -- Part 2, 45 degree line so the difference between x and y will be the same.
+      local number_of_loops = math.abs(current_x-end_x) 
+      -- Start i at 0 so that we don't miss the last point of the line
+      for i=0, number_of_loops, 1 do
+      -- In/decrement X, and Y from starting to ending point and record it on the map
+         map[current_y][current_x] =  map[current_y][current_x] + 1
+         if is_x_increasing then
+            current_x = current_x + 1
+         else
+            current_x = current_x - 1
+         end
+         if is_y_increasing then
+            current_y = current_y + 1
+         else
+            current_y = current_y - 1
+         end
       end
    end
 
    return map
 end
 
-local function part_one(file_name)
+local function solution(file_name, is_part_two)
    local lines = get_lines_from_input(file_name)
-   lines = remove_non_straight_lines(lines)
+   if not is_part_two then
+      lines = remove_non_straight_lines(lines)
+   end
    local map = generate_map_from_lines(lines)
    for _,line in ipairs(lines) do
       map = add_line_to_map(line,map)
@@ -92,8 +121,16 @@ local function part_one(file_name)
 end
 
 print("Sample Part 1")
-part_one("sample.txt")
+solution("sample.txt",false)
 
 print("-------------------------")
 print("Input Part 1")
-part_one("input.txt")
+solution("input.txt",false)
+
+print("-------------------------")
+print("Sample Part 2")
+solution("sample.txt",true)
+
+print("-------------------------")
+print("Input Part 2")
+solution("input.txt",true)
